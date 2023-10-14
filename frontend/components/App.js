2483ad12d8aa18d3abc8309eb526c23859d015e0
Shane A. Healy <ShaneAHealy55@gmail.com>
2023-10-14 17:13:33 -0500

    testing trial code

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,23 +1,81 @@
-import React from 'react'
-import axios from 'axios'
-import Character from './Character'
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import Character from './Character';
 
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
+const urlPlanets = 'http://localhost:9009/api/planets';
+const urlPeople = 'http://localhost:9009/api/people';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+  const [people, setPeople] = useState([]);
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+  const setData = () => {
+    Promise
+      .all([
+        Axios.get(urlPlanets), 
+        Axios.get(urlPeople)
+      ])
+      .then(resp => {
+        setPlanets(resp[0].data);
+        setPeople(resp[1].data);
+      });
+  }
+  useEffect(setData,[]);
+  console.log(people);
+  console.log(planets);
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {people.map(char => {
+        const planetId = char.homeworld;
+        const planetName = planets.find(planet => planet.id === planetId).name;
+        return(
+          <Character key={char.id} name={char.name} homeworld={{id: planetId, name: planetName}} />
+        )
+      })}
     </div>
   )
 }
 
-export default App
+export default App;
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
 if (typeof module !== 'undefined' && module.exports) module.exports = App
+
+
+//wrong code sample
+
+
+
+
+//Default Starting Point
+
+
+
+//import React from 'react'
+//import axios from 'axios'
+//import Character from './Character'
+
+//const urlPlanets = 'http://localhost:9009/api/planets'
+//const urlPeople = 'http://localhost:9009/api/people'
+
+//function App() {
+  // ❗ Create state to hold the data from the API
+  // ❗ Create effects to fetch the data and put it in state
+//  return (
+ //   <div>
+ //     <h2>Star Wars Characters</h2>
+ //     <p>See the README of the project for instructions on completing this challenge</p>
+ //     {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+ //   </div>
+ // )
+//}
+
+//export default App
+
+//// ❗ DO NOT CHANGE THE CODE  BELOW
+//if (typeof module !== 'undefined' && module.exports) module.exports = App
+
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Character() { // ❗ Add the props
+function Character(props) { // id, name, homeworld: {id, name}
+  const [active, setActive] = useState(false);
+
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const handleClick = () => setActive(!active);
+
+  //efective code
+
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className='character-card' onClick={handleClick}>
+      <h3 className='character-name'>{props.name}</h3>
+      {active ? <p>Planet: <span className='character-planet'>{props.homeworld.name}</span></p> : null}
     </div>
   )
 }
 
-export default Character
+export default Character;
+
+
+
+// Alternative Solution, Commented Out
+
+
+
+
+
+//Default Starting Point
+
+
+//import React from 'react'
+
+//function Character() { // ❗ Add the props
+//  // ❗ Create a state to hold whether the homeworld is rendering or not
+//  // ❗ Create a "toggle" click handler to show or remove the homeworld
+//  return (
+ //   <div>
+ //     {/* Use the same markup with the same attributes as in the mock */}
+ //   </div>
+//  )
+//}
+
+//export default Character
+
+
+
